5e06981ead122553df221e26567fa8afc44d11c3
Gabriel-Lyra-Santos-CEUB <160976293+Gabriel-Lyra-Santos-CEUB@users.noreply.github.com>
2024-10-31 22:00:15 -0300

    Add files via upload
    adiciona esqueleto trabalho final

---
+++ BlazorVendasBCCTN/Entities/Pedido.cs
@@ -0,0 +1,14 @@
+﻿namespace BlazorVendasBCCTN.Entities
+{
+    public class Pedido
+    {
+        public int Id { get; set; }
+        public DateTime DataPedido { get; set; }
+        public int FuncionarioId { get; set; }
+        public Funcionario Funcionario { get; set; }
+        public int ClienteId { get; set; }
+        public Cliente Cliente { get; set; }
+        public double ValorTotal { get; set; }
+        public ICollection<ItemVenda> ItensVenda { get; set; }
+    }
+}
